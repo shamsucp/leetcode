b538e394797fe7f38a26055b7724224ad6ae733f
shmasucp <shamsudheencp7@gmail.com>
2024-10-25 17:10:36 +0530

    variable changed 2

--- twosum.py
+++ twosum.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for x in range(len(nums)):
-            for y in range(x+1,len(nums)):
-                if nums[x]+nums[y]==target:
-                    return x,y
+            for j in range(x+1,len(nums)):
+                if nums[x]+nums[j]==target:
+                    return x,j
